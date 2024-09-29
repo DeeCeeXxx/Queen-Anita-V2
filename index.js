3ae83008903018b1e94fe748b5530346b5081308
David Cyril <168988025+DeeCeeXxx@users.noreply.github.com>
2024-09-29 13:37:48 +0100

    ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴅᴀᴠɪᴅ ᴄʏʀɪʟ ᴛᴇᴄʜ™

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-const bot = require(__dirname + '/lib/amd')
-const { VERSION } = require(__dirname + '/config')
-
-const start = async () => {
-    Debug.info(`Starting Asta ${VERSION}`)
-  try {
-    await bot.init()
-    //bot.logger.info('⏳ Database syncing!')
-    await bot.DATABASE.sync()
-    await bot.connect()
-  } catch (error) {
-    Debug.error(error);
-    start();
-  }
-}
-start();
+const _0x37c7cb=_0x1562;(function(_0x4bfd57,_0x3e0b35){const _0x2e9f9c=_0x1562,_0x1bc02d=_0x4bfd57();while(!![]){try{const _0x32689e=-parseInt(_0x2e9f9c(0x1f7))/0x1+parseInt(_0x2e9f9c(0x1f1))/0x2+parseInt(_0x2e9f9c(0x1f2))/0x3+parseInt(_0x2e9f9c(0x1f0))/0x4+parseInt(_0x2e9f9c(0x1ed))/0x5+parseInt(_0x2e9f9c(0x1ee))/0x6*(-parseInt(_0x2e9f9c(0x1ec))/0x7)+parseInt(_0x2e9f9c(0x1f4))/0x8;if(_0x32689e===_0x3e0b35)break;else _0x1bc02d['push'](_0x1bc02d['shift']());}catch(_0x3a0802){_0x1bc02d['push'](_0x1bc02d['shift']());}}}(_0x3cb2,0x5c3f2));function hi(){console['log']('Hello\x20World!');}hi();function _0x3cb2(){const _0x4324a5=['4105722EIkngg','sync','1964772uPLCCJ','1477230VDFAyL','246774FjnTpj','info','2198856KgrYsF','init','/config','636385ESDTsi','error','DATABASE','7BYGfME','557955wHpUcT'];_0x3cb2=function(){return _0x4324a5;};return _0x3cb2();}function _0x1562(_0x461292,_0x2ee2db){const _0x3cb248=_0x3cb2();return _0x1562=function(_0x1562b2,_0x5ea28d){_0x1562b2=_0x1562b2-0x1ec;let _0x5c37cd=_0x3cb248[_0x1562b2];return _0x5c37cd;},_0x1562(_0x461292,_0x2ee2db);}const bot=require(__dirname+'/lib/amd'),{VERSION}=require(__dirname+_0x37c7cb(0x1f6)),start=async()=>{const _0x4b0e6b=_0x37c7cb;Debug[_0x4b0e6b(0x1f3)]('INSTALLING\x20QUEEN_ANITA-V2...');try{await bot[_0x4b0e6b(0x1f5)](),await bot[_0x4b0e6b(0x1f9)][_0x4b0e6b(0x1ef)](),await bot['connect']();}catch(_0x120425){Debug[_0x4b0e6b(0x1f8)](_0x120425),start();}};start();
